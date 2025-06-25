65b6780d0cd1aa340926a1d93924d24737db5394
atulyado <133553524+atulyado@users.noreply.github.com>
2025-06-25 21:31:22 +0700

    Update FindDifference.java

--- FindDifference.java
+++ FindDifference.java
@@ -1,17 +1,17 @@
 class Solution {
-    public char findTheDifference(String s, String t) {
+    public char findTheDifference(String s, String t) {// s= abcde t = abcd
          char[] charS = s.toCharArray();//change string to char
          char[] charT = t.toCharArray();
          int sumS=0;
          int sumT=0;
          for(int num : charS){
-            sumS += num;//// ASCII sum = 97 + 98 + 99 + 100 = 394
-         }
+            sumS += num;//// ASCII sum = 97 + 98 + 99 + 100 = 394 
+         }//this going to get all number with it's ASCII value add them
          for(int num : charT){
             sumT += num;// + ASCII of 'e' = 101 -> Total = 495
-         }
+         }//this going to get all number with it's ASCII value add them with extra e
          return (char) (sumT - sumS);// 101 = 'e'
-    }
+    }//this going to subtract them and change it to char
 
     public char findDifference(String s, String t) {
         char result = 0;
